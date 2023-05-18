f9603584e4718485800e57f5435ee4e578fa2783
Ennak-alt <aj72kanne@gmail.com>
2023-05-18 11:26:32 +0200

    Temporary data

--- load.sql
+++ load.sql
@@ -1,26 +1,29 @@
 DROP TABLE IF EXISTS Post;
 
 CREATE TABLE Post (
-    carid VARCHAR(50),
+    idx INT,
+    id VARCHAR(50),
     region VARCHAR(50),
     price INT,
-    year INT,
+    caryear VARCHAR(50),
     manufacturer VARCHAR(50),
-    model VARCHAR(50)
+    model VARCHAR(50),
     condition VARCHAR(50),
-    cylinders INT,
+    cylinders VARCHAR(50),
     fuel VARCHAR(50),
-    odometer INT,
+    odometer VARCHAR(50),
     transmission VARCHAR(50),
     VIN VARCHAR(50),
     drive VARCHAR(50),
     size VARCHAR(50),
     cartype VARCHAR(50),
     paint_color VARCHAR(50),
-    cardescription VARCHAR(500),
+    cardescription VARCHAR(100000),
     county VARCHAR(50),
     carstate VARCHAR(2),
     posting_date VARCHAR(50)
-)
+);
 
-COPY Post FROM '/cars.csv' DELIMITER ',';
+COPY Post FROM '/Users/axelkanne/Documents/Go/go-next/cars.csv' DELIMITER ';';
+
+select * from Post limit 1
