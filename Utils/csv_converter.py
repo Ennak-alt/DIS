0db75a13e754379401d6bc63baba94295f0a5f17
Ennak-alt <aj72kanne@gmail.com>
2023-06-02 11:53:52 +0200

    Cleaned csv

--- Utils/csv_converter.py
+++ Utils/csv_converter.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import math
 
 df = pd.read_csv('vehicles.csv')
 
@@ -9,6 +10,47 @@ df.pop('lat')
 df.pop('long')
 df.pop('title_status')
 
-df["description"] = "abc"
+newdf = []
+for index, row in df.iterrows(): 
+    if str(row['region']) == "nan":
+        df.at[index, 'region'] = "auburn"
+    if str(row['year']) == "nan":
+        df.at[index, 'year'] = 2010
+    df.at[index, 'year'] = int(df['year'][index])
+    if str(row['manufacturer']) == "nan":
+        df.at[index, 'manufacturer'] = "gmc"
+    if str(row['model']) == "nan":
+        df.at[index, 'model'] = "unknown"
+    if str(row['condition']) == "nan":
+        df.at[index,'condition'] = "good"
+    if str(row['cylinders']) == "nan":
+        df.at[index, 'cylinders'] = 6
+    elif row['cylinders'][0].isdigit():
+        df.at[index, 'cylinders'] = int(row['cylinders'][0])
+    else: 
+        df.at[index, 'cylinders'] = 8
+    if str(row['fuel']) == "nan":
+        df.at[index, 'fuel'] = "other"
+    if str(row['odometer']) == "nan":
+        df.at[index,'odometer'] = 0
+    # df.at['odometer'] = int(df['odometer'][index])
+    if str(row['transmission']) == "nan":
+        df.at[index, 'transmission'] = "other"
+    if str(row['drive']) == "nan":
+        df.at[index, 'drive']= "rwd"
+    if str(row['size']) == "nan":
+        df.at[index, 'size'] = "mid-size"
+    if str(row['type']) == "nan":
+        df.at[index, 'type'] = "other"
+    if str(row['paint_color']) == "nan":
+        df.at[index, 'paint_color'] = "blue"
+    if str(row['description']) != "nan":
+        df.at[index, 'description'] = row['description'].replace(';', '')
+    if index == 10100:
+        break
+newdf = df[27:].head(10000)
+newdf['year'] = newdf['year'].fillna(0).astype(int)
+newdf['odometer'] = newdf['odometer'].fillna(0).astype(int)
+newdf['cylinders'] = newdf['cylinders'].fillna(0).astype(int)
 
-print(df.head(2000).to_csv("cars.csv", sep = ';'))
+print(newdf.to_csv("cars.csv", sep = ';'))
